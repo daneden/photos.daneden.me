001d0ea2321f845cc18df9a404a2957b4888ec53
Daniel Eden <dan.eden@me.com>
2017-01-26 11:26:25 +0000

    Enable vertical scrolling in browsers that support mousewheel events

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import './index.css';
 import imageData from './manifest.js';
 import SiteInfo from './meta';
 
+
 imageData.reverse();
 
 ReactDOM.render(
   <App preface={SiteInfo.fullDescription} images={imageData} />,
   document.getElementById('root')
 );
+
+// change vertical scroll to horizontal in content
+let content = document.querySelector('.site-content')
+window.addEventListener('mousewheel', MouseWheelHandler);
+
+function MouseWheelHandler(e) {
+  if(content === undefined) {
+    content = document.querySelector('.site-content')
+  }
+
+  content.scrollTo(content.scrollLeft + e.deltaY, 0)
+}
