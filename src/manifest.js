bcc1ae932ea96b2a3369ac783093fb3d6245dd27
Daniel Eden <dan.eden@me.com>
2018-12-03 15:20:34 -0800

    Un-ignore manifest file to fix flow error

---
+++ src/manifest.js
@@ -0,0 +1,390 @@
+const imageData = [
+ {
+  "aspectRatio": 0.666726683471372,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00002.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/320"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 2,
+  "fileName": "00003.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/100"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00004.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/3200"
+ },
+ {
+  "aspectRatio": 0.6667265218172024,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00005.jpg",
+  "focalLength": "19.0mm",
+  "iso": 200,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00006.jpg",
+  "focalLength": "19.0mm",
+  "iso": 125,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 1.4998720900486058,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00007.jpg",
+  "focalLength": "19.0mm",
+  "iso": 640,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 2,
+  "fileName": "00008.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/1600"
+ },
+ {
+  "aspectRatio": 1.4998624484181569,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00009.jpg",
+  "focalLength": "19.0mm",
+  "iso": 250,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 0.6667239305961175,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00010.jpg",
+  "focalLength": "19.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/1000"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 4,
+  "fileName": "00011.jpg",
+  "focalLength": "19.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/80"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 4,
+  "fileName": "00012.jpg",
+  "focalLength": "19.0mm",
+  "iso": 500,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 1.4998735777496839,
+  "camera": "Sony α6000",
+  "fStop": 4,
+  "fileName": "00013.jpg",
+  "focalLength": "19.0mm",
+  "iso": 200,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 18,
+  "fileName": "00014.jpg",
+  "focalLength": "50.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/15"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00015.jpg",
+  "focalLength": "19.0mm",
+  "iso": 200,
+  "shutterSpeed": "1/40"
+ },
+ {
+  "aspectRatio": 0.6667241379310345,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00016.jpg",
+  "focalLength": "19.0mm",
+  "iso": 200,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 10,
+  "fileName": "00017.jpg",
+  "focalLength": "19.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/125"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00018.jpg",
+  "focalLength": "50.0mm",
+  "iso": 320,
+  "shutterSpeed": "1/80"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00019.jpg",
+  "focalLength": "19.0mm",
+  "iso": 1250,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 0.6666083304165208,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00020.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/3200"
+ },
+ {
+  "aspectRatio": 0.6667241082198863,
+  "camera": "Sony α6000",
+  "fStop": 9,
+  "fileName": "00021.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/320"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00022.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/4000"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00023.jpg",
+  "focalLength": "35.0mm",
+  "iso": 400,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 0.6456888888888889,
+  "camera": "Sony α6000",
+  "fStop": 13,
+  "fileName": "00024.jpg",
+  "focalLength": "19.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/100"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00025.jpg",
+  "focalLength": "35.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/3200"
+ },
+ {
+  "aspectRatio": 1.4998720900486058,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00026.jpg",
+  "focalLength": "19.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/200"
+ },
+ {
+  "aspectRatio": 1.4998740236835475,
+  "camera": "Sony α6000",
+  "fStop": 4,
+  "fileName": "00027.jpg",
+  "focalLength": "19.0mm",
+  "iso": 320,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 4,
+  "fileName": "00028.jpg",
+  "focalLength": "19.0mm",
+  "iso": 125,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 3.2,
+  "fileName": "00029.jpg",
+  "focalLength": "19.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/250"
+ },
+ {
+  "aspectRatio": 1.5026178010471205,
+  "camera": "Sony α6000",
+  "fStop": 2.8,
+  "fileName": "00030.jpg",
+  "focalLength": "19.0mm",
+  "iso": 200,
+  "shutterSpeed": "1/4000"
+ },
+ {
+  "aspectRatio": 0.6666039510818439,
+  "camera": "Sony α6000",
+  "fStop": 8,
+  "fileName": "00031.jpg",
+  "focalLength": "12.0mm",
+  "iso": 100,
+  "shutterSpeed": 8
+ },
+ {
+  "aspectRatio": 1.4998704327545997,
+  "camera": "Sony α6000",
+  "fStop": 4.5,
+  "fileName": "00032.jpg",
+  "focalLength": "19.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/250"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00033.jpg",
+  "focalLength": "24.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/3000"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00034.jpg",
+  "focalLength": "24.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/250"
+ },
+ {
+  "aspectRatio": 1.4998737692501893,
+  "camera": "Sony α6000",
+  "fStop": 4.5,
+  "fileName": "00035.jpg",
+  "focalLength": "24.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 0.6667284866468842,
+  "camera": "Sony α6000",
+  "fStop": 1.8,
+  "fileName": "00036.jpg",
+  "focalLength": "24.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/4000"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 16,
+  "fileName": "00037.jpg",
+  "focalLength": "12.0mm",
+  "iso": 400,
+  "shutterSpeed": "1/200"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Sony α6000",
+  "fStop": 16,
+  "fileName": "00038.jpg",
+  "focalLength": "12mm",
+  "iso": 160,
+  "shutterSpeed": "1/60"
+ },
+ {
+  "aspectRatio": 1.4998603741971517,
+  "camera": "Leica Q",
+  "fStop": 1.7,
+  "fileName": "00039.jpg",
+  "focalLength": "28.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/12000"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Leica Q",
+  "fStop": 1.7,
+  "fileName": "00040.jpg",
+  "focalLength": "28.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/250"
+ },
+ {
+  "aspectRatio": 0.666796875,
+  "camera": "Leica Q",
+  "fStop": 1.7,
+  "fileName": "00041.jpg",
+  "focalLength": "28.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/6400"
+ },
+ {
+  "aspectRatio": 0.6667519836191451,
+  "camera": "Leica Q",
+  "fStop": 1.7,
+  "fileName": "00042.jpg",
+  "focalLength": "28.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/16000"
+ },
+ {
+  "aspectRatio": 1.5,
+  "camera": "Leica Q",
+  "fStop": 9,
+  "fileName": "00043.jpg",
+  "focalLength": "28.0mm",
+  "iso": 250,
+  "shutterSpeed": "1/250"
+ },
+ {
+  "aspectRatio": 0.6666666666666666,
+  "camera": "Leica Q",
+  "fStop": 1.7,
+  "fileName": "00044.jpg",
+  "focalLength": "28.0mm",
+  "iso": 100,
+  "shutterSpeed": "1/1000"
+ }
+];
+    export default imageData;
