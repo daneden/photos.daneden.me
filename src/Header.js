39429cc2e0e148837f47adb70fb63340a25e30f4
Daniel Eden <dan.eden@me.com>
2019-02-16 14:36:47 -0800

    Further CSS cleanups

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import * as React from "react"
 
 const Header = (): React.Node => {
   return (
-    <header className="site-header u-mbm">
+    <header className="site-header u-mb">
       <h1 className="site-title">
         <a href="https://daneden.me">Daniel Eden</a> &mdash; Photos
       </h1>
